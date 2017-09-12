2e0ad1f076225c0d4596b8a59ac0e7967aa2920f
Zabarovka <Zabarovka@users.noreply.github.com>
2017-09-12 14:15:13 +0300

    Update ConcaveShape.hpp

--- ConcaveShape.hpp
+++ ConcaveShape.hpp
@@ -11,21 +11,18 @@ namespace sfml {
 
 class ConcaveShape: public sf::Drawable, public sf::Transformable {
   private:
-    std::vector<sf::VertexArray> triangles;
-    std::vector<sf::Vector2f> points;
     const sf::Texture* texture;
     sf::Color color;
-    float Area() const;
+    std::vector<sf::VertexArray> triangles;
+    void Triangulate(const std::vector<sf::Vector2f>& points);
+    float Area(const std::vector<sf::Vector2f>& points) const;
     bool PointOnLine(sf::Vector2f pt,sf::Vector2f v1,sf::Vector2f v2) const;
     bool PointInTriangle(sf::Vector2f p,sf::Vector2f p1,sf::Vector2f p2,sf::Vector2f p3) const;
-    void CreateTriangle(unsigned int a,unsigned int b,unsigned int c);
-    void TriangulateInner(unsigned int n,std::vector<unsigned int>& p);
+    void CreateTriangle(unsigned int a,unsigned int b,unsigned int c,const std::vector<sf::Vector2f>& points);
   public:
-    explicit ConcaveShape(sf::Color c=sf::Color::Black);
-    explicit ConcaveShape(const sf::Texture& t,sf::Color c=sf::Color::Black);
+    explicit ConcaveShape(const std::vector<sf::Vector2f>& points,sf::Color c=sf::Color::Black);
+    ConcaveShape(const std::vector<sf::Vector2f>& points,const sf::Texture& t,sf::Color c=sf::Color::Black);
     virtual ~ConcaveShape();
-    void addPoint(sf::Vector2f point);
-    void Triangulate();
     void setColor(sf::Color c);
     void setTexture(const sf::Texture& t);
     virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
